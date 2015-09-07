672ccd5520927e4c8e25ff2b92863bd68cd52f8c
Victor <bocharsky.bw@gmail.com>
2015-09-07 12:38:34 +0300

    Init project

---
+++ test.php
@@ -0,0 +1,47 @@
+
+<?php
+
+/*
+Printing
+some
+string
+*/
+// echo 'Hello <span style="color: green;">world</span>!<br>';
+
+// string: always in quotes
+$name = 'Victor';
+// echo $name;
+
+// integer: without quotes
+$count = 5;
+
+// float: without quotes
+$price = 99.99;
+
+// available operations: * / + -
+$totalPrice = $count * $price;
+
+// concatenation with "." sign
+// echo 'Total price: ' . $totalPrice . '$';
+echo "Total price: \"$totalPrice\" $<br>";
+
+// boolean: true false
+$isBlocked = false;
+
+if ($isBlocked) {
+	echo "$name, you are blocked!<br>";
+} else {
+	echo "$name, welcome!<br>";
+}
+
+if ($totalPrice >= 1000 or $totalPrice == 500) {
+	echo 'Your discount: 25%<br>';
+} elseif ($totalPrice >= 600) {
+	echo 'Your discount: 15%<br>';
+} elseif ($totalPrice >= 400) {
+	echo 'Your discount: 10%<br>';
+} else {
+	echo 'Your discount: 5%<br>';
+}
+
+?>
